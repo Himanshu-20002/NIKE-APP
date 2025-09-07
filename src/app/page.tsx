b4ddbfb5e59ba8cc581cbb18ec6516bc49d3495b
Heichi <heichi.ux@gmail.com>
2025-09-07 11:02:10 +0530

    refined optmized for production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-import LiquidChrome from '../component/LiquidChrome'
-import Hero from '../component/Hero'
+
+
+
 import ReactLenis from "lenis/react"
-import Nav from '../components/Nav'
-import IconiCollection from '../component/IconiCollection'
-import { Footer,CustomerReviews,SpecialOffer,SupperQuality,Subscribe,Services,PopularProduct} from '../sections'
+
+
+import { Footer,SupperQuality,Subscribe,PopularProduct} from '../sections'
 import HeroPage from "../component/Heropage"
 import Section from "@/component/Section";
-import Scene from "@/components/3d/scene";
+
 
 
 export default function Home() {
@@ -22,9 +22,7 @@ export default function Home() {
 
 <Subscribe/>
 <Footer/>
- {/* <div className=" h-100vh  bg-black  w-full  relative   ">
-         <Scene/>
-        </div> */}
+
 
 
 
